3afaed1630ccc27ce2d210338fac32843462116c
Chris Oakman <chris@oakmac.com>
2023-02-21 00:16:02 -0600

    saving progress

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Pretty print, validate, and minify EDN files from within Sublime Text.
 
 ## How it works
 
-- Require babashka installed
+- Require [jet](https://github.com/borkdude/jet/) to be installed
+- Optionally fall back to just babashka?
 - Shell out to nodejs
 - Use Tonsky's Python Clojure parser from Python directly?
 
@@ -12,25 +13,16 @@ Pretty print, validate, and minify EDN files from within Sublime Text.
 
 ### Package Control
 
-If you have [Package Control] installed, you can easily install the
-[Parinfer](https://packagecontrol.io/packages/Parinfer) package:
-
-1. In Sublime Text, open the Command Palette by typing <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>p</kbd>
-   (<kbd>Cmd</kbd>+<kbd>Shift</kbd>+<kbd>p</kbd> on Mac)
-2. Type `install` and select `Package Control: Install Package`
-3. A text prompt should appear shortly after Package Control loads a list of
-   packages from the Internet.
-4. Type `parinfer` and press <kbd>Enter</kbd>
-5. That's it! Parinfer is now installed.
+FIXME: write this
 
 ### Linux / OSX
 
-You can symlink the the parinfer repo to the Sublime Text Packages directory.
+You can symlink this package repo to the Sublime Text Packages directory:
 
 ```
 cd ~
-git clone git@github.com:oakmac/sublime-text-parinfer.git
-ln -s ~/sublime-text-parinfer ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Parinfer
+git clone git@github.com:oakmac/sublime-text-edn.git
+ln -s ~/sublime-text-edn ~/Library/Application\ Support/Sublime\ Text/Packages/
 ```
 
 ## Development
--- sublime-text-edn.py
+++ sublime-text-edn.py
@@ -1,14 +1,51 @@
 import sublime
 import sublime_plugin
+import shutil
+import os
+import subprocess
 
 class PrettyEdnFormat(sublime_plugin.TextCommand):
     def run(self, edit):
-        print("pretty EDN format 99999999")
+        whole_view_region = sublime.Region(0, self.view.size())
+        all_text = self.view.substr(whole_view_region)
+
+        # TODO: allow them to set babashka path via setting
+        bb_exists = shutil.which("bb")
+        jet_exists = shutil.which("jet")
+
+        if not jet_exists:
+        	print("jet not found!")
+        	return
+
+        #print("do something now")
+
+        # subprocess.run(args, *, stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False, cwd=None, timeout=None, check=False, encoding=None, errors=None, text=None, env=None, universal_newlines=None, **other_popen_kwargs)
+        result = subprocess.run(
+            ["jet", "--to", "edn", all_text], capture_output=True, text=True
+        )
+        print("stdout:", result.stdout)
+        print("stderr:", result.stderr)
+
+        ## Possibles:
+        ## - os.system('bb foo')
+        ## - os.spawn
+        ## - subprocess.run
 
 class PrettyEdnMinify(sublime_plugin.TextCommand):
     def run(self, edit):
-        print("pretty EDN minify 11111")
+        print("FIXME: pretty EDN minify")
+        one = shutil.which("cat")
+        two = shutil.which("bb")
+
+        if one:
+        	print("yes to one")
+
+        if two:
+        	print("yes to two")
 
 class PrettyEdnValidate(sublime_plugin.TextCommand):
     def run(self, edit):
-        print("pretty EDN validation 22222")
+        whole_view_region = sublime.Region(0, self.view.size())
+        all_text = self.view.substr(whole_view_region)
+        print(all_text)
+        self.view.replace(edit, whole_view_region, all_text + "zzz")
